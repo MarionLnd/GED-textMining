86dde2181306b9557c3f7d2ccf93540ea0024c64
marieme <ps.marieme95@gmail.com>
2021-05-19 20:51:31 +0200

    - Récupération des fichiers - Ajout dans la base

--- src/back/server.js
+++ src/back/server.js
@@ -1,5 +1,10 @@
 const mariadb = require("mariadb");
 const express = require("express");
+const PDFExtract = require('pdf.js-extract').PDFExtract;
+const pdfExtract = new PDFExtract();
+const options = {}; /* see below */
+const testFolder = '/Users/user/Desktop/CoursENTP';
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -20,22 +25,48 @@ const connectionOptions = {
 app.get("/", async function (req, res) {
 	console.log("MARIADB Connection config:");
 	console.log(connectionOptions);
-
 	const pool = mariadb.createPool(connectionOptions);
 	let connection;
-
 	try {
 		connection = await pool.getConnection();
-
 		let queryStr = "SELECT * FROM USER";
 		let rows = await connection.query(queryStr);
-
 		res.send(rows);
+		// Récupération des chemins fichier
+		fs.readdir(testFolder, (err, files) => {
+			files.forEach(file => {
+				pdfExtract.extract('/Users/user/Desktop/CoursENTP/'+file, options, (err, data) => {
+					if (err) return console.log(err);
+				});
+			});
+		});
 	} catch (err) {
 		throw err;
 	} finally {
 		if (connection) return connection.release();
 	}
+});
+
+app.get("/nouveauDocument", async function (req, res) {
+	pool.getConnection()
+		.then(conn => {
+			conn.query("SELECT 1 as val")
+				.then((rows) => {
+					return conn.query('INSERT INTO CATEGORY value (?, ?)', [1, "2017"]);
+				})
+				.then((res) => {
+					console.log(res);
+					conn.end();
+				})
+				.catch(err => {
+					//handle error
+					console.log(err);
+					conn.end();
+				})
+
+		}).catch(err => {
+		//not connected
+	});
 });
 
 app.listen(port, function () {
