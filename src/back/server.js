e4ecf932038c037feb090dfc5f76f92106ffd9aa
MarionLnd <maye43@gmail.com>
2021-05-20 23:12:25 +0200

    Merging script with docker

--- src/back/server.js
+++ src/back/server.js
@@ -1,5 +1,10 @@
 const mariadb = require("mariadb");
 const express = require("express");
+const PDFExtract = require("pdf.js-extract").PDFExtract;
+const fs = require("fs");
+const pdfExtract = new PDFExtract();
+const options = {}; /* see below */
+const testFolder = "/var/www/html/";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -20,22 +25,152 @@ const connectionOptions = {
 app.get("/", async function (req, res) {
 	console.log("MARIADB Connection config:");
 	console.log(connectionOptions);
-
 	const pool = mariadb.createPool(connectionOptions);
 	let connection;
-
 	try {
 		connection = await pool.getConnection();
-
-		let queryStr = "SELECT * FROM USER";
+		let queryStr =
+			"INSERT INTO USER VALUE ('ieuhcouqfvhvoubf', 'logintest', 'loginpwd', true)";
 		let rows = await connection.query(queryStr);
 
 		res.send(rows);
+		console.log(rows);
+
+		fs.readdir(testFolder, (err, files) => {
+			console.log(files);
+		});
 	} catch (err) {
 		throw err;
 	} finally {
 		if (connection) return connection.release();
 	}
+});
+
+app.get("/nouveauDocument", async function (req, res) {
+	const pool = mariadb.createPool(connectionOptions);
+	pool
+		.getConnection()
+		.then((conn) => {
+			conn
+				.query("SELECT 1 as val")
+				.then((rows) => {
+					/* Récupération des chemins fichier */
+					fs.readdir(testFolder, (err, files) => {
+						if (files !== undefined) {
+							files.forEach((file) => {
+								pdfExtract.extract(testFolder + file, options, (err, data) => {
+									if (err) return console.log(err);
+									if (data.meta.metadata !== null) {
+										Object.entries(data.meta.metadata).forEach(
+											([key, value]) => {
+												var donnees = [];
+												donnees.push(file);
+												donnees.push(file);
+												if (data.meta.metadata[key]["dc:title"] === undefined)
+													donnees.push(null);
+												else donnees.push(data.meta.metadata[key]["dc:title"]);
+												if (
+													data.meta.metadata[key]["xmp:createdate"] ===
+													undefined
+												)
+													donnees.push(null);
+												else
+													donnees.push(
+														data.meta.metadata[key]["xmp:createdate"].substring(
+															0,
+															10
+														)
+													);
+												if (
+													data.meta.metadata[key]["xmp:modifydate"] ===
+													undefined
+												)
+													donnees.push(null);
+												else
+													donnees.push(
+														data.meta.metadata[key]["xmp:modifydate"].substring(
+															0,
+															10
+														)
+													);
+												if (data.meta.metadata[key]["dc:creator"] === undefined)
+													donnees.push(null);
+												else
+													donnees.push(data.meta.metadata[key]["dc:creator"]);
+												donnees.push(null);
+												donnees.push(testFolder + file);
+												donnees.push(null);
+												if (
+													data.meta.metadata[key]["xmpmm:documentid"] ===
+													undefined
+												)
+													donnees.push(null);
+												else
+													donnees.push(
+														data.meta.metadata[key]["xmpmm:documentid"]
+													);
+												donnees.push(null);
+												donnees.push(null);
+												console.log(donnees);
+												// console.log(data.meta.metadata[key]['dc:title'])
+												conn.query(
+													"INSERT INTO DOCUMENT value (?,?,?,?,?,?,?,?,?,?,?,?)",
+													[
+														donnees[0],
+														donnees[1],
+														donnees[2],
+														donnees[3],
+														donnees[4],
+														donnees[5],
+														donnees[6],
+														donnees[7],
+														donnees[8],
+														donnees[9],
+														donnees[10],
+														donnees[11],
+													]
+												);
+											}
+										);
+									} else {
+										console.log("Pas de métadonnées");
+										conn.query(
+											"INSERT INTO DOCUMENT value (?,?,?,?,?,?,?,?,?,?,?,?)",
+											[
+												file,
+												file,
+												null,
+												null,
+												null,
+												null,
+												0,
+												testFolder + file,
+												null,
+												null,
+												null,
+												null,
+											]
+										);
+									}
+								});
+							});
+						}
+					});
+				})
+				.then((res) => {
+					console.log(res);
+					conn.end();
+				})
+				.catch((err) => {
+					//handle error
+					console.error(err);
+					conn.end();
+				});
+		})
+		.catch((err) => {
+			//not connected
+			console.error("Error: Not connected - ", err);
+		});
 });
 
 app.listen(port, function () {
